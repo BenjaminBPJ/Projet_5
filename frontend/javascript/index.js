3a0ef2f839f612460fc822a5e0824dd7206c7a0c
BenjaminBPJ <pierrejeanbenjamin@hotmail.fr>
2020-10-30 00:34:46 +0100

    creation du choix de couleur

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,28 +1,24 @@
-fetch (`http://localhost:3000/api/teddies`) // connection à l'API
-    .then(function(response){
+fetch(`http://localhost:3000/api/teddies`) // connection à l'API
+    .then(function (response) {
         return response.json()
     })
-    .then(function(data){  // vérfication que l'on a bien les données du JSON
+    .then(function (data) {  // vérfication que l'on a bien les données du JSON
         console.log(data);
 
-            data.forEach(function(value){ // création des cartes produits
-                let article = document.createElement(`a`)
-                document.querySelector("main").appendChild(article)
-                article.classList.add("card")
-                article.setAttribute(`href`, `produit.html?id=${value._id}`)
+        data.forEach(function (value) { // création des cartes produits
+            let article = document.createElement(`a`)
+            document.querySelector("main").appendChild(article)
+            article.classList.add("card")
+            article.setAttribute(`href`, `produit.html?id=${value._id}`)
 
-                    article.innerHTML = `<h3>${value.name}</h3>
+            article.innerHTML = `<h3>${value.name}</h3>
                                          <img src= ${value.imageUrl} class="teddy"/>
                                          <p>${value.description}</p>
                                          <p><strong>Couleurs : </strong>${value.colors}</p>
-                                         <p><strong>Prix : </strong>${value.price/100} €</p>`
+                                         <p><strong>Prix : </strong>${value.price / 100} €</p>`
+        })
     })
-    })
-    .catch((err)=>console.log(`pas de serveur:${err}`));
-
-
-
-    
+    .catch((err) => console.log(`pas de serveur:${err}`));
 
 
 
--- frontend/javascript/product.js
+++ frontend/javascript/product.js
@@ -1,26 +1,39 @@
-let getId = window.location.search
-let urlParam = getId.replace("?id=", "")
+let getId = window.location.search  // avoir l'Id de connection
+let urlParam = getId.replace("?id=", "") // connecter l'ID à la page
 
-let id = function (){ 
-return urlParam
+let id = function () {
+    return urlParam
 }
 
 let url = `http://localhost:3000/api/teddies/`
-fetch(url + urlParam) // connection à l'API
-    .then(function(response){
+fetch(url + urlParam) // connection à l'API avec l'ID du bon produit
+    .then(function (response) {
         return response.json()
     })
-    .then(function(data){  // vérfication que l'on a bien les données du JSON
+    .then(function (data) {  // vérfication que l'on a bien les données du JSON
         console.log(data);
 
         let article = document.createElement(`article`)
         document.querySelector("main").appendChild(article)
         article.classList.add("card-produit")
 
-            article.innerHTML = `<h3>${data.name}</h3>
-                                 <img src= ${data.imageUrl} class="teddy"/>
-                                 <p>${data.description}</p>
-                                 <p><strong>Prix : </strong>${data.price/100} €</p>
-                                 <button>Mettre dans mon panier</button>`
-                      
-        })
+        article.innerHTML = `<h3>${data.name}</h3> 
+                             <img src= ${data.imageUrl} class="teddy"/>
+                             <p>${data.description}<p>
+                             <p><strong>Couleur : </strong></p>
+                             <select></select>
+                             <p><strong>Prix : </strong>${data.price / 100} €</p>
+                             <p><strong>Quantité :</strong><input class="quantite"></input></p>
+                             <button>Mettre dans mon panier</button>`
+
+
+        for (let i = 0; i < data.colors.length; i = i + 1) {   // boucle pour ajouter les bonnes couleurs pour chaque ourson
+            let option = document.createElement('option')
+            document.querySelector('main article select').appendChild(option)
+            option.setAttribute("value", data.colors[i])   // permet d'avoir les choix multiples pour chaque couleurs grâce à l'attribut donné
+
+            option.innerHTML = `${data.colors[i]}`
+
+        }
+    }
+    )
